99d12216f9a7553067f655c08c4ab4427f556015
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-07-11 22:10:10 +0300

    Edit History Card

--- src/components/Sidebar/HistoryCard.js
+++ src/components/Sidebar/HistoryCard.js
@@ -1,21 +1,27 @@
 import { NavLink } from 'react-router-dom';
-import { useSelectedChat } from '../../contexts/userContext';
+import { useSelectedChat } from '../../contexts/userContext.js';
 
 const HistoryCard = ({ userData }) => {
 
-    const { selectedUserId } = useSelectedChat();
+    const { selectedUserId, selectUserId } = useSelectedChat();
+
     console.log('selected: ', selectedUserId);
 
     return (
-        <NavLink to={`/messages/${userData.userId}`}>
-            <div className="sidebar__history__cell noselect ">
-                <div className="sidebar__history__cell__img_wp">
-                    <img src={userData.imageUrl} alt="User image" />
-                </div>
-                <div className="sidebar__history__cell__wrapper">
-                    <p className="sidebar__history__cell__username">{userData.name}</p>
-                    <p className="sidebar__history__cell__message">{userData.lastMessage}</p>
-                </div>
+        <NavLink
+            to={`/messages/${userData.userId}`}
+            className={({ isActive, isPending }) =>
+                isActive
+                    ? 'sidebar__history__cell noselect active'
+                    : 'sidebar__history__cell noselect'
+            }
+        >
+            <div className="sidebar__history__cell__img_wp">
+                <img src={userData.imageUrl} alt="User image" />
+            </div>
+            <div className="sidebar__history__cell__wrapper">
+                <p className="sidebar__history__cell__username">{userData.name}</p>
+                <p className="sidebar__history__cell__message">{userData.lastMessage}</p>
             </div>
         </NavLink>
     )
