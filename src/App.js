96b6c535415f09bd3393efb876c709fcfc976825
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-09-14 23:15:53 +0300

    Fixed bug with routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import Auth from './components/Auth/Auth.js';
-import Main from './components/Main/Main.js';
+import Chat from './components/Chat/Chat.js';
 import NotFound from './components/NotFound/NotFound';
 import { Route, Routes } from 'react-router-dom';
+import Chatarea from './components/Chatarea/Chatarea.js';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Auth />} />
-        <Route path='/chat' element={<Main />} />
-
+        <Route path='chat' element={<Chat />} >
+          <Route path='messages/:userId' element={<Chatarea />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
