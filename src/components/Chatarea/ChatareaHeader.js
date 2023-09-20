27a0cb0194e7d95f1c360768d6ceb7a3d5588ff0
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-09-20 22:50:30 +0300

    Add ChatareaHeader component

---
+++ src/components/Chatarea/ChatareaHeader.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import { useSelectedUser } from '../../contexts/SelectedUserContext';
+
+const ChatareaHeader = ({ setIsVisible }) => {
+    
+    const { selectedUser, setSelectedUser } = useSelectedUser();
+
+    return (
+        <div className="chatarea__header">
+            <div className="chatarea__header__user">
+                <div className="user-img-wp">
+                    <img src={selectedUser.imageUrl} alt="User img" />
+                </div>
+                <div className="user-info">
+                    <h3>{selectedUser.firstName + ' ' + selectedUser.lastName}</h3>
+                    <p>На линия в момента</p>
+                </div>
+            </div>
+            <div className="chatarea__header__icons">
+                <div className="icons_wp">
+                    <span className="circle">
+                        <i className="fa-solid fa-phone" />
+                    </span>
+                    <span className="circle">
+                        <i className="fa-solid fa-video" />
+                    </span>
+                    <span className="circle" onClick={() => setIsVisible()}>
+                        <i className="fa-solid fa-circle-info" />
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ChatareaHeader;
