e7e6c19b796c1ad8cad29669848f0ae03ccbdbef
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-09-20 22:58:19 +0300

    dynamically loaded messages functionality

--- src/components/Chatarea/Chatarea.js
+++ src/components/Chatarea/Chatarea.js
@@ -2,146 +2,49 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { getChatMessagesByRoomId } from '../../services/chatService';
+import { getUserById } from '../../services/userService';
+
+import ChatareaHeader from './ChatareaHeader';
+import ChatareaMessages from './ChatareaMessages';
+import ChatareaUserActions from './ChatareaUserActions';
 import ProfileInfo from '../ProfileInfo/ProfileInfo';
+import { useSelectedUser } from '../../contexts/SelectedUserContext';
+import { useAuth } from '../../contexts/AuthContext';
 
 const Chatarea = () => {
     const { roomId } = useParams();
+
+    const { user } = useAuth();
+    const { selectedUser, setSelectedUser } = useSelectedUser();
+
     const [isProfileVisible, setIsProfileVisible] = useState(false);
-    const [chatMessages, setChatMessages] = useState();
+    const [roomData, setRoomData] = useState();
+
 
     useEffect(() => {
         const loadHistoryData = async () => {
-            const messages = await getChatMessagesByRoomId(roomId);
-            setChatMessages(messages);
-            console.log('Chat Messages: ',chatMessages);
+            // return object {chatMessages, users} 
+            const room = await getChatMessagesByRoomId(roomId);
+            const chatRoomFriendId = room.users.filter(u => u._id != user.id)[0]._id;
+            const friendInfo = await getUserById(chatRoomFriendId);
+            setSelectedUser(friendInfo);
+            setRoomData(room);
         }
         loadHistoryData();
-    }, []);
+    }, [roomId]);
 
 
-    function setisVisible() {
+    function setIsVisible() {
         setIsProfileVisible(!isProfileVisible);
-    }
-
-    function sendMessage(e) {
-        if (e.key === "Enter") {
-            console.log(e.target);
-            e.target.value = '';
-        }
     }
 
     return (
         <>
             <section className="chatarea">
                 <div className="chatarea_container">
-                    <div className="chatarea__header">
-                        <div className="chatarea__header__user">
-                            <div className="user-img-wp">
-                                <img src="imgs/user_img.jpg" alt="User img" />
-                            </div>
-                            <div className="user-info">
-                                <h3>Велислав Боянов</h3>
-                                <p>На линия в момента</p>
-                            </div>
-                        </div>
-                        <div className="chatarea__header__icons">
-                            <div className="icons_wp">
-                                <span className="circle">
-                                    <i className="fa-solid fa-phone" />
-                                </span>
-                                <span className="circle">
-                                    <i className="fa-solid fa-video" />
-                                </span>
-                                <span className="circle" onClick={() => setisVisible()}>
-                                    <i className="fa-solid fa-circle-info" />
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="chatarea__messages">
-                        <div className="chatarea__messages__wrapper">
-                            <div className="message_row user">
-                                <div className="message_row__content uslm">asdf</div>
-                            </div>
-                            <div className="message_row fr">
-                                <span className="circle">
-                                    <img src="imgs/user_img.jpg" alt="" />
-                                </span>
-                                <div className="message_row__content fflm">asdf</div>
-                            </div>
-                            <div className="message_row fr ">
-                                <span className="circle">
-                                    <img src="imgs/user_img.jpg" alt="" />
-                                </span>
-                                <div className="message_row__content fllm">asdf</div>
-                            </div>
-                            <div className="message_row user ">
-                                <div className="message_row__content uflm">asdf</div>
-                            </div>
-                            <div className="message_row user ">
-                                <div className="message_row__content ullm">asdf</div>
-                            </div>
-                            <div className="message_row fr">
-                                <span className="circle">
-                                    <img src="imgs/user_img.jpg" alt="" />
-                                </span>
-                                <div className="message_row__content fflm">asdf</div>
-                            </div>
-                            <div className="message_row fr">
-                                <span className="circle">
-                                    <img src="imgs/user_img.jpg" alt="" />
-                                </span>
-                                <div className="message_row__content fmlm">asdf</div>
-                            </div>
-                            <div className="message_row fr">
-                                <span className="circle">
-                                    <img src="imgs/user_img.jpg" alt="" />
-                                </span>
-                                <div className="message_row__content fllm">asdf</div>
-                            </div>
-                            <div className="message_row user">
-                                <div className="message_row__content uflm">asdf</div>
-                            </div>
-                            <div className="message_row user">
-                                <div className="message_row__content  umlm">asdf</div>
-                            </div>
-                            <div className="message_row user ">
-                                <div className="message_row__content ullm">asdf 123</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="chatarea__useraction">
-                        <div className="useraction_container noselect">
-                            <span className="circle">
-                                <i className="fa-solid fa-circle-plus" />
-                            </span>
-                            <span className="circle">
-                                <i className="fa-solid fa-images" />
-                            </span>
-                            <span className="circle">
-                                <i className="fa-solid fa-note-sticky" />
-                            </span>
-                            <span className="circle">
-                                <i>GIF</i>
-                            </span>
-                        </div>
-                        <div className="useraction__input">
-                            <input
-                                className="input"
-                                type="text"
-                                placeholder="Aa"
-                                onKeyDown={sendMessage}
-                            />
-                            <span className="cont-height">
-                                <i className="fa-solid fa-face-smile" />
-                            </span>
-                        </div>
-                        <div className="useraction__like-btn">
-                            <span className="circle">
-                                <i className="fa-solid fa-thumbs-up" />
-                            </span>
-                        </div>
-                    </div>
+                    <ChatareaHeader setIsVisible={setIsVisible} />
+                    <ChatareaMessages roomData={roomData} />
+                    <ChatareaUserActions />
                 </div>
             </section>
             {
