cc945ebe9639a050da67a64fc0dfbcd36c9294e2
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-09-22 16:36:20 +0300

    Added dropdown click handler

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,44 @@
+import { useState } from 'react'
+import { useAuth } from '../../contexts/AuthContext';
+import HeaderDropdown from './HeaderDropdown';
+
 const Header = () => {
+    const { user } = useAuth();
+    const [isVisible, setIsVisible] = useState(false);
+
+    function onClickDropdown() {
+        return setIsVisible(!isVisible)
+    }
     return (
         <header className="header">
             <div className="nav_wrapper left">
-                <img
-                    src="imgs/Facebook_Logo_(2019).svg"
-                    className="header__logo"
-                    alt="Logo"
-                />
-                <span className="circle">
+                <h1>chatbook</h1>
+                <span className="circle--big">
                     <i className="fa-solid fa-magnifying-glass" />
                 </span>
             </div>
             <div className="nav_wrapper right">
-                <span className="circle">
+                <span className="circle--big">
                     <i className="fa-solid fa-bell" />
                 </span>
-                <span className="circle">
+                <span className="circle--big" onClick={onClickDropdown}>
                     <img
                         className="circle__img"
-                        src="https://ik.imagekit.io/8brpz6ecl/vasilignatov_dev/IMG_0631.JPG?updatedAt=1687415333369"
+                        // TODO
+                        src={
+                            user.imageUrl
+                                ? user.imageUrl
+                                : "https://ik.imagekit.io/8brpz6ecl/vasilignatov_dev/IMG_0631.JPG?updatedAt=1687415333369"
+                        }
                         alt="profile image"
                     />
                 </span>
             </div>
+            
+            {
+                isVisible && <HeaderDropdown  />
+            }
+
         </header>
     )
 }
