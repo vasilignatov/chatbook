1302f1e4a33cf97bbf7a3a27b1a5da0721cc94a6
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-09-23 11:45:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import Auth from './components/Auth/Auth.js';
+import { useEffect, useState } from 'react';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+
 import Chat from './components/Chat/Chat.js';
 import NotFound from './components/NotFound/NotFound';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import Chatarea from './components/Chatarea/Chatarea.js';
-import NoSelectedChat from './components/Chatarea/NoSelectedChat.js';
+import NoSelectedChat from './components/Chatarea/NoSelectedChat/NoSelectedChat';
+import { socket } from './socket.js';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path='/' element={<Chat />}>
         <Route index element={<NoSelectedChat />} />
-        <Route path='messages/:userId' element={<Chatarea />} />
+        <Route path='messages/:roomId' element={<Chatarea />} />
       </Route>
 
       <Route path="*" element={<NotFound />} />
@@ -19,6 +21,29 @@ const router = createBrowserRouter(
 )
 
 function App() {
+
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+    }
+
+  }, []);
+
   return (
     <>
       <RouterProvider router={router} />
