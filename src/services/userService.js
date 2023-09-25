0abaaf12eaecb41b5b9e52321dd7e43a87f32604
Vasil <vasilignatow@gmail.com>
2023-09-25 22:00:04 +0300

    Added getSuggestions request

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,3 +4,8 @@ import { request, getOptions } from './utils.js';
 export async function getUserById(userId) {
     return await request(endpoints.getUserById + userId, getOptions());
 }
+
+export async function getSuggestions(string) {
+    return await request(endpoints.getSuggestions + string, getOptions());
+}
+
