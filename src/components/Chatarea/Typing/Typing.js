e156d9f1c2bf37d8430c867c3918c34034f29616
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-10-10 21:20:11 +0300

    Create Typing.js

---
+++ src/components/Chatarea/Typing/Typing.js
@@ -0,0 +1,15 @@
+import './Typing.css';
+
+const Typing = () => {
+    return (
+        <div className="message_row fr">
+            <div className="message_row__content uslm">
+                <div className="typing__dot"></div>
+                <div className="typing__dot"></div>
+                <div className="typing__dot"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Typing;
